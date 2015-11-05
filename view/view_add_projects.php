4c41342a0a522e076f3fc6e1e087783221196c0b
Isaac Mast <isaac.mast@emu.edu>
2015-11-05 11:44:23 -0500

    Changed echo to eos to convert echo content to HTML

--- view/view_add_projects.php
+++ view/view_add_projects.php
@@ -3,7 +3,11 @@ class view_page {
 
 public function view_add_projects($project)
 {
-	echo "<center><h1>Add a Project</h1></center><p>If a client does not appear in the dropdown list as you type, add the client first, and then add the project.</p><br><form method="POST"><strong>Start typing in client name and select from the dropdown menu</strong><input type="text" placeholder="Type client name if already in database"><br>Location<textarea></textarea><br>Description<textarea></textarea><br>Data received (YYYY-MM-DD):<input type="text" placeholder="YYYY-MM-DD"><br><input type="submit" value="Add new project"></form>";
+	echo <<<eos
+
+<center><h1>Add a Project</h1></center><p>If a client does not appear in the dropdown list as you type, add the client first, and then add the project.</p><br><form method="POST"><strong>Start typing in client name and select from the dropdown menu</strong><input type="text" placeholder="Type client name if already in database"><br>Location<textarea></textarea><br>Description<textarea></textarea><br>Data received (YYYY-MM-DD):<input type="text" placeholder="YYYY-MM-DD"><br><input type="submit" value="Add new project"></form>;
+
+eos;
 }
 }
 ?>
