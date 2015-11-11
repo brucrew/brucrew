909b7955a0dd171772a431c225eb272911583413
Isaac Mast <isaac.mast@emu.edu>
2015-11-11 01:28:04 -0500

    Changed formatting of page layout

--- view/view_add_projects.php
+++ view/view_add_projects.php
@@ -5,7 +5,32 @@ public function view_add_projects()
 {
 	echo <<<eos
 
-<center><h1>Add a Project</h1></center><p>If a client does not appear in the dropdown list as you type, add the client first, and then add the project.</p><br><form method="POST"><strong>Start typing in client name and select from the dropdown menu</strong><input type="text" placeholder="Type client name if already in database"><br>Location<textarea></textarea><br>Description<textarea></textarea><br>Data received (YYYY-MM-DD):<input type="text" placeholder="YYYY-MM-DD"><br><input type="submit" value="Add new project"></form>;
+<center>
+	<h1>Add a Project</h1>
+</center>
+<p>If a client does not appear in the dropdown list as you type, add the client first, and then add the project.</p>
+<br>
+<form method="POST"><strong>Start typing in client name and select from the dropdown menu</strong>
+<br>
+<input type="text" placeholder="Type client name if already in database">
+<br>
+<br>
+<strong>Location</strong>
+<br>
+<textarea></textarea>
+<br>
+<br>
+<strong>Description</strong>
+<br>
+<textarea></textarea>
+<br>
+<br>
+<strong>Date received (YYYY-MM-DD):</strong>
+<br>
+<input type="text" placeholder="YYYY-MM-DD">
+<br>
+<br>
+<input type="submit" value="Add new project"></form>
 
 eos;
 }
