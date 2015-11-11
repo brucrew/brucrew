a898fee91dabfb5487ec1e9034d73b3c15a0f918
Isaac Mast <isaac.mast@emu.edu>
2015-11-11 01:07:08 -0500

    Added nav bar link to clients page

--- template/header.php
+++ template/header.php
@@ -80,7 +80,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Clients<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">All Clients</a></li>
+            <li><a href="clients.php">All Clients</a></li>
             <li><a href="#">My Regular Clients</a></li>
 	    <li><a href="#">Add New Client</a></li>
           </ul>
